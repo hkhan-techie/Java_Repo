4d3fa69d7b6000bed669fc920b38d670f7ed2d3d
Hidayathulla Khan <hkhan.techie@gmail.com>
2024-07-27 15:00:54 +0530

    Exception handling implementations.

---
+++ Java_Examples/src/com/mywork/exception/ExceptionExample.java
@@ -0,0 +1,71 @@
+package com.mywork.exception;
+
+public class ExceptionExample {
+    public static void main(String[] args) {
+        handleException();
+
+        try {
+            throwsException();
+        } catch (Exception e) {
+            System.out.println("Throws exception :" + e);
+        }
+
+        int result = finallyException();
+        System.out.println("result: " + result);
+
+        try {
+            customException();
+        } catch (CustomException e) {
+            System.out.println("CustomException :" + e.getMessage());
+        }
+
+
+    }
+
+    private static void handleException() {
+        int x = 5;
+        int y = 0;
+        try {
+            System.out.println(x / y);
+        } catch (ArithmeticException e) {
+            System.out.println("Exception :" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void throwsException() throws ArithmeticException {
+        int x = 5;
+        int y = 0;
+        try {
+            System.out.println(x / y);
+        } catch (ArithmeticException e) {
+            throw e;
+        }
+    }
+
+    private static int finallyException() {
+        int x = 5;
+        int y = 0;
+        int result;
+        try {
+            result = x / y;
+            return result;
+        } catch (ArithmeticException e) {
+            System.out.println(" finally Exception :" + e.getMessage());
+        } finally {
+            System.out.println("Executing finally block");
+            result = 0;
+        }
+        return result;
+    }
+
+    private static void customException() throws CustomException {
+        int x = 5;
+        int y = 0;
+        try {
+            System.out.println(x / y);
+        } catch (ArithmeticException e) {
+            throw new CustomException(e.getMessage());
+        }
+    }
+}
